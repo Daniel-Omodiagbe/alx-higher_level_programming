74a0445c2dbc7598cce79ec782878d91de994715
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-01-17 01:29:54 +0100

    a function that checks for lowercase character

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    """checks for lower case characters"""
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    if c in lower:
+        return True
+    else:
+        return False
