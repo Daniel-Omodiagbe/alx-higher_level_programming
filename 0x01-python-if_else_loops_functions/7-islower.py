92cbcbcb27fdce9fd313b20f4c2fa96af4915a57
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-01-17 01:44:17 +0100

    made correction

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     """checks for lower case characters"""
-    lower = "abcdefghijklmnopqrstuvwxyz"
-    if c in lower:
+    if c >= chr(97) and c <= chr(123):
         return True
     else:
         return False
