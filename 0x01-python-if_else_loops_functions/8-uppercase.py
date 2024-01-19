123f2045604ee9162a83b28a887b6f3e7cadbda9
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-01-19 01:55:07 +0100

    made correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def islower(c):
+def uppercase(str):
     """checks for upper case characters"""
+    for i in str:
+        if i
     if c >= chr(65) and c <= chr(90):
         return True
     else:
