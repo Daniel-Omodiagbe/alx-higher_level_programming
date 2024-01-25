398d5d0d121c9a23ff3f7fdd8e844fba63cf53bd
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-01-25 16:13:18 +0100

    made correction

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,7 +6,9 @@ def only_diff_elements(set_1, set_2):
 
     new_list = []
     for i in set_1:
-        new_list.append(i)
+        if i not in set_2:
+            new_list.append(i)
     for i in set_2:
-        new_list.append(i)
+        if i not in set_1:
+            new_list.append(i)
     return set(new_list)
