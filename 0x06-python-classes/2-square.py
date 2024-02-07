a56f5f3b79b760704f47c8c1913ad9298d47ebda
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-02-07 03:38:58 +0100

    define attributes the getter and setter methods in python

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module defines the square class using the 1-square.py"""
+
+
+class Square:
+    """class that defines a square with private instance
+    attribute size and instantiation with size
+    size must be an integer, exceptions will also be handled"""
+    def __init__(self, size=0):
+        """instantiation of attributes"""
+        self.size = size
+
+    @property
+    def size(self):
+        """getting size"""
+        return self.__size
+
+    @size.setter
+    def size(self, x):
+        """sets the size attribute"""
+        if type(x) is not int:
+            raise TypeError("size must be an integer")
+        if x < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = x
