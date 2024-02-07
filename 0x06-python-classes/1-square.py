4a42a7698cd15d6784b6a7a49a6aa6d074117a81
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-02-07 03:08:23 +0100

    the class Square has been given an instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines the square class based on 0-square.py"""
+
+
+class Square:
+    """class that defines a square with private instance
+    attribute size and instantiation with size"""
+    def __init__(self, size):
+        """instantiation of attributes"""
+        self.size = size
