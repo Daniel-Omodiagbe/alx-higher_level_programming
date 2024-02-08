924262fdea3fc6510065eab694bdefb07f3d7085
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-02-08 14:03:12 +0100

    made correction

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     """function that divides element by element 2 lists"""
-    max_length = max([len(my_list_1), len(my_list_2)])
     new_list = []
-    for i in range(max_length):
+    for i in range(list_length):
         result = 0
         try:
             result = (my_list_1[i] / my_list_2[i])
