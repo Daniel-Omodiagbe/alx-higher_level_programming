0e069e7f2b7bc9f0f6e135593413bd07e7c4222e
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-02-09 08:09:17 +0100

    made correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,5 +8,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as err:
-        stderr.write(f"Exception: {err} \n")
+        stderr.write(f"Exception: {err}\n")
     return False
