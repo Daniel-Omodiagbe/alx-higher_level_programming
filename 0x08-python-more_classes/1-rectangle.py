79fb8d830b9da48189811efb4aaa36235ed0fad1
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-02-13 08:57:13 +0100

    a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This module defines a rectangle class based on 0-rectangle
+"""
+
+
+class Rectangle:
+    """ defines a rectangle based on 0-rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """ instantiation of class """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ retrieves the width attribute """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ sets the value of width attribute """
+        if value not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ retrieves the width attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ sets the value of width attribute """
+        if value not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
