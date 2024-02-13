36739377346f8f4611c208be0c4465d045bad7f8
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-02-13 09:19:30 +0100

    a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""a class rectangle"""
+"""defines the class rectangle"""
 
 
 class Rectangle:
-    """ defines a rectangle based on 0-rectangle"""
+    """ defines a rectangle based on 1-rectangle
+    with width and height as attribute"""
 
     def __init__(self, width=0, height=0):
         """ instantiation of class """
@@ -37,3 +38,13 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """ returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
