afb77650bfaa8c4a1c5aa4a9d2541bec20dc9efe
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-02-25 23:19:21 +0100

    update the rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,3 +70,17 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the rectangle using #"""
+        for i in range(self.__height):
+            print("#" * self.__width)
+
+    def __str__(self):
+        """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+        {self.__width}/{self.__height}"
