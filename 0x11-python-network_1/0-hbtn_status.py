fac746352b08cdd1ddfa06ac835696e65e66fb77
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-05-12 01:29:34 +0100

    a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A python script that fetches a url status
+"""
+
+import urllib.request
+
+
+req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+with urllib.request.urlopen(req) as response:
+    res = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(res)}")
+    print(f"\t- content: {res}")
+    print(f"\t- utf8 content: {(res.decode('utf-8'))}")
