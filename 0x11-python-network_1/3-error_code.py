b5ede98a0a62e2743deefae55ae3497d8bfbfff7
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-05-12 09:07:19 +0100

    made correction

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
         with urll.urlopen(req) as res:
             print(res.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        print("Error code: {}"format(e.code))
