13b91c596852677f2a0cf637c089e4ec08d37ff2
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-05-21 10:22:33 +0100

    simple implementation of a serialization and deserialization mechanism

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for class Student"""
+
+
+class Student:
+    """class that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a student"""
+        attrs_dict = {}
+        if isinstance(attrs, list):
+            if all(isinstance(item, str) for item in attrs):
+                for i in attrs:
+                    if hasattr(self, i):
+                        attrs_dict[i] = getattr(self, i)
+                return attrs_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """that replaces all attributes of the Student instance"""
+        for key, val in json.items():
+            setattr(self, key, val)
