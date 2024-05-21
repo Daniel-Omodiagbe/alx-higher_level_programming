a6b2d6534ea247cfdbe72c70df7cc92bfca3dcba
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-05-21 09:06:55 +0100

    Hotfix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """module for class_to_json"""
 
-import json
-
 
 def class_to_json(obj):
     """function that returns the dictionary description
     with simple data structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object"""
     if type(obj) is obj.__class__:
-        return json.dumps(obj.__dict__)
+        return (obj.__dict__)
