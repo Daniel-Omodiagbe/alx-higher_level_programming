48c40eb093c59f8a78296cc3a918234f13101a5b
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-05-30 12:47:58 +0100

    a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- script that lists all the cities of California
+-- that can be found in the database hbtn_0d_usa
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+state_id INT NOT NULL,
+FOREIGN KEY (state_id) REFERENCES states(id),
+name VARCHAR(256));
