b858ebfa55a180416b0581a2f9731ff9f4ce8de1
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-06-07 15:51:05 +0100

    Quickfix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,17 +9,18 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("""SELECT cities.name FROM cities\
+    cur.execute("""SELECT states.name, cities.name FROM cities\
                 LEFT JOIN states ON state_id = states.id\
                 where states.name LIKE %s\
                 ORDER BY cities.id ASC;""", (argv[4],))
     cities1 = cur.fetchall()
     length = len(cities1)
     for i in range(length):
-        cityy = str(cities1[i]).strip("(),'")
-        if i < length - 1:
-            print(cityy, end=', ')
-        else:
-            print(cityy)
+        if cities1[i][0] == argv[4]:
+            cityy = str(cities1[i][1]).strip("(),'")
+            if i < length - 1:
+                print(cityy, end=', ')
+            else:
+                print(cityy)
     cur.close()
     conn.close()
